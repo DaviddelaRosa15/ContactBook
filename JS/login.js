0d19a7d4e9fa7a3e8eed4f441047b38b4bd2e612
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2022-08-12 00:07:48 -0400

    Cambié el boton de show por un icono toggle

--- JS/login.js
+++ JS/login.js
@@ -1,22 +1,22 @@
-function login(){
+function login() {
     let user = document.getElementById('user').value;
     let password = document.getElementById('pass').value;
-    let userArray = ["Zaiz","Brian","David","Ruth"];
-    let passArray = ["z1234","b1234","d1234","r1234"];
+    let userArray = ["Zaiz", "Brian", "David", "Ruth"];
+    let passArray = ["z1234", "b1234", "d1234", "r1234"];
     let success = false;
 
-    for(var i=0;i<userArray.length;i++){
-        if((user == userArray[i]) && password == passArray[i]){
+    for (var i = 0; i < userArray.length; i++) {
+        if ((user == userArray[i]) && password == passArray[i]) {
             success = true;
             break;
         }
     }
 
-    if(success){
+    if (success) {
         alert("Welcome: " + user);
         window.location.href = './Crud.html';
     }
-    else{
+    else {
         alert("Wrong user or password, please check");
         document.getElementById('user').value = "";
         document.getElementById('pass').value = "";
@@ -24,11 +24,14 @@ function login(){
     }
 }
 
-function seePass(){
-    var seePassword = document.getElementById("pass");
-    if(seePassword.type == "password"){
-        seePassword.type = "text";
-    }else{
-        seePassword.type = "password";
-    }
-  }
+function seePass() {
+    const togglePassword = document.querySelector("#togglePassword");
+    const password = document.querySelector("#password");
+
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    togglePassword.classList.toggle("bi-eye");
+
+}
